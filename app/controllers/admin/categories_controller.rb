eff92358a3a3c75c9dd8325c15ab0d1b025cefc6
Frederick <frederick.gfl@gmail.com>
2020-02-06 18:59:52 +0000

    Put links to categories in table

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,5 +1,9 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: Rails.configuration.admin_login[:name],
+                               password: Rails.configuration.admin_login[:password]
+
   def index
+    @categories = Category.order(id: :desc).all
   end
 
   def new
