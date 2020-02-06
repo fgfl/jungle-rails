5f1c0821020b8331d488edfb7b0af6db1e9bab83
Frederick <frederick.gfl@gmail.com>
2020-02-06 18:11:10 +0000

    Add links to the products and categories on Admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,5 +5,6 @@ class Admin::DashboardController < ApplicationController
   def show
     @product_count = Product.count
     @category_count = Category.count
+    @categories = Category.all
   end
 end
