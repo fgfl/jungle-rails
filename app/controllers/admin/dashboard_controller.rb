ed786fe301f8028cf1a443881d96de1ea30e2003
Frederick <frederick.gfl@gmail.com>
2020-02-06 01:11:20 +0000

    Show product count and category count

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,5 +3,7 @@ class Admin::DashboardController < ApplicationController
                                password: Rails.configuration.admin_login[:password]
 
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 end
