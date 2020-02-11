3efb91dc683233d4b8c3fb0a75357b7c896d9cdb
Frederick <frederick.gfl@gmail.com>
2020-02-11 03:31:06 +0000

    Add user validation and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe User, type: :model do
   describe "Validations:" do
-    it "the user is valid with proper fields" do
+    before(:each) do
       @user = User.new(
         first_name: "Joe",
         last_name: "Shmoe",
@@ -10,8 +10,76 @@ RSpec.describe User, type: :model do
         password: "test",
         password_confirmation: "test",
       )
-      # raise @user.inspect
+    end
+
+    it "the user is valid with proper fields" do
       expect(@user).to be_valid
     end
+
+    it "is required to have a password" do
+      @user.password = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("Password can't be blank")).to be_truthy
+    end
+
+    it "is required to have a password confirmation" do
+      @user.password_confirmation = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("Password confirmation can't be blank")).to be_truthy
+    end
+
+    it "is required to have an email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("Email can't be blank")).to be_truthy
+    end
+
+    it "is required to have a first name" do
+      @user.first_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("First name can't be blank")).to be_truthy
+    end
+
+    it "is required to have a last name" do
+      @user.last_name = nil
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("Last name can't be blank")).to be_truthy
+    end
+
+    it "is invalid if password != password_confirmation" do
+      @user.password_confirmation = "else"
+      expect(@user).to_not be_valid
+      expect(@user.errors.full_messages.include?("Password confirmation doesn't match Password")).to be_truthy
+    end
+
+    it "should have a unique email" do
+      @user.save
+      @user2 = User.new(
+        first_name: "Joe",
+        last_name: "Shmoe",
+        email: "Joe@shmoe.com",
+        password: "test",
+        password_confirmation: "test",
+      )
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages.include?("Email has already been taken")).to be_truthy
+    end
+
+    it "should have case insensitive emails" do
+      @user.save
+      @user2 = User.new(
+        first_name: "Joe",
+        last_name: "Shmoe",
+        email: "JOE@SHMOE.COM",
+        password: "test",
+        password_confirmation: "test",
+      )
+      expect(@user2).to_not be_valid
+      expect(@user2.errors.full_messages.include?("Email has already been taken")).to be_truthy
+    end
+  end
+
+  describe "Password" do
+    it "is greater than the minimum length"
   end
 end
