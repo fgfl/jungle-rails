121fb47944cec02addac95fc613caa70e0a43158
Frederick <frederick.gfl@gmail.com>
2020-02-11 20:30:18 +0000

    Save emails to db in lowercase only.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params[:user][:email].downcase!
     params.require(:user).permit(
       :first_name,
       :last_name,
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,14 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  before_save :email_downcase
+
+  def email_downcase
+    self.email.downcase!
+  end
+
   def self.authenticate_with_credentials(email, password)
-    puts email.downcase.strip
     user = User.find_by_email(email.downcase.strip)
-    # raise user.inspect
     if user && user.authenticate(password)
       return user
     else
