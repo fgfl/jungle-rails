69c042c9c8776a9580f7e8d11860527380eeccd9
Frederick <frederick.gfl@gmail.com>
2020-02-11 00:53:45 +0000

    Add password_confirmation to params when signin up;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,10 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
+    @user = User.new(user_params)
 
-    if user.save
-      session[:user_id] = user.id
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to products_path
     else
       render :new
@@ -21,7 +21,8 @@ class UsersController < ApplicationController
       :first_name,
       :last_name,
       :email,
-      :password
+      :password,
+      :password_confirmation
     )
   end
 end
