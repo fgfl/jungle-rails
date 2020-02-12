d26fa6cef480a1e56d9bffcecb9e0ec2f6ad1deb
Frederick <frederick.gfl@gmail.com>
2020-02-12 00:17:20 +0000

    Test cart updates the item count

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.feature "Visitor adds a product to cart", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset("apparel1.jpg"),
+        quantity: 10,
+        price: 64.99,
+      )
+    end
+  end
+
+  scenario "The cart updates to show the item count" do
+    visit root_path
+
+    expect(page.find("#navbar")).to have_link("My Cart (0)")
+
+    page.first("article.product").click_button("Add")
+
+    expect(page.find("#navbar")).to have_link("My Cart (1)")
+  end
+end
